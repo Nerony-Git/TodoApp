e8b39599bbd410d134185fad035094688ec7e79b
George Amuzu <gnamuzu@gmail.com>
2023-04-26 22:25:26 +0100

    updated the servlet to direct to login page

--- src/main/java/com/george/todoapp/HelloServlet.java
+++ src/main/java/com/george/todoapp/HelloServlet.java
@@ -1,26 +1,64 @@
 package com.george.todoapp;
 
 import java.io.*;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
+import com.george.todoapp.dao.LoginDao;
+import com.george.todoapp.model.User;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private static final long serialVersionUID = 1L;
+    private LoginDao loginDao;
 
     public void init() {
-        message = "Hello World!";
+        loginDao = new LoginDao();
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("index.jsp");
+    }
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        authenticate(request, response);
+    }
+
+    private void authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            User user = loginDao.validate(username, password);
+            String destPage = "index.jsp";
+
+            if (user != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", user);
+                Enumeration<String> attributeNames = session.getAttributeNames();
+                while (attributeNames.hasMoreElements()){
+                    String attributeName = attributeNames.nextElement();
+                    Object attributeValue = session.getAttribute(attributeName);
+                    System.out.println(attributeName + ": " + attributeValue);
+                }
+                destPage = "/list";
+            }else {
+                String message = "Invalid username or password";
+                request.setAttribute("message", message);
+            }
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+            dispatcher.forward(request, response);
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new ServletException(ex);
+        }
+
     }
 
     public void destroy() {
